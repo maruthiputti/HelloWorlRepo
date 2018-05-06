77aade03133f85522b662406dabcf3252608410b
Maruthi Prasad Putti <mputti@teleflora.org>
2018-05-06 17:18:41 +0530

    updated hello

--- Hello1.java
+++ Hello1.java
@@ -2,7 +2,7 @@ public class Hello1{
 
 public static void main(String args[])
 {int i;
-for(i=0;i<21;i++){System.out.println("hi ......."+i);}
+for(i=0;i<22;i++){System.out.println("hi ......."+i);}
 
 }
 }
